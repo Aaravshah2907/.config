52f9490f2453856653f34fed23b2dfb1a7535591
Aarav Shah <74358472+Aaravshah2907@users.noreply.github.com>
2026-03-29 16:25:22 +0530

    Added spot for audio,video, regular files. Also added tags to Books.

--- yazi/init.lua
+++ yazi/init.lua
@@ -30,6 +30,7 @@ require("simple-tag"):setup({
 	  ["!"] = "#cc9057", -- Pin
 	  -- ["1"] = "cyan",
 	  ["p"] = "red", -- Protected
+	  ["i"] = "#595f61", -- Images
 	  ["I"] = "red", -- Important
 	  ["B"] = "yellow", -- Books
 	  ["T"] = "blue", -- Travel
@@ -50,6 +51,7 @@ require("simple-tag"):setup({
 
     -- format: [tag key] = "tag icon"
 		["x"] = "",
+		["i"] = "",
 		["c"] = "",
 		["!"] = "",
 		["p"] = "",
@@ -64,3 +66,27 @@ require("simple-tag"):setup({
   },
 
 })
+
+require('spot'):setup {
+  metadata_section = {
+    enable = true,
+    hash_cmd = 'xxhsum', -- other hashing commands may be slower
+    hash_filesize_limit = 150, -- in MB, set 0 to disable
+    relative_time = true, -- 2026-01-01 or n days ago
+    time_format = '%Y-%m-%d %H:%M', -- https://www.man7.org/linux/man-pages/man3/strftime.3.html
+    show_compression = 'size', ---@type false|"size"|"percentage"
+  },
+  plugins_section = {
+    enable = true,
+  },
+  style = {
+    section = 'green',
+    key = 'reset',
+    value = 'blue',
+    selected = 'blue',
+    colorize_metadata = true,
+    height = 20,
+    width = 60,
+    key_length = 15,
+  },
+}

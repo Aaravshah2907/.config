2a01e31ed53af48f86d7901a26dd5d3a1970154b
Aarav Shah <74358472+Aaravshah2907@users.noreply.github.com>
2026-06-21 23:27:48 +0530

    chore : reverted back to default settings

---
+++ yazi/plugins/max-preview.yazi/main.lua
@@ -0,0 +1,9 @@
+local function load_init()
+    local init_path = "/Users/aaravshah2975/.config/yazi/plugins/max-preview.yazi/init.lua"
+    return dofile(init_path)
+end
+
+local plugin = load_init()
+return {
+    entry = plugin.entry
+}

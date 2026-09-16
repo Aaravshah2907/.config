86a9655b53cf0135e77c9124933a9fc05c7e13d1
Aarav Shah <74358472+Aaravshah2907@users.noreply.github.com>
2026-09-16 18:32:47 +0530

    feat: updated the sript

--- nvim/lua/plugins/cpkb.lua
+++ nvim/lua/plugins/cpkb.lua
@@ -4,9 +4,30 @@ return {
   dependencies = { "nvim-telescope/telescope.nvim" },
   config = function()
     -- Load the custom extension from cpkb
-    require("telescope").load_extension("cpkb")
+    pcall(require("telescope").load_extension, "cpkb")
+
+    -- Add commands
+    vim.api.nvim_create_user_command("CPKBAddVisual", function()
+      require("cpkb.add").add_from_visual()
+    end, { desc = "Add snippet to CPKB from visual selection" })
+
+    vim.api.nvim_create_user_command("CPKBAddYank", function()
+      require("cpkb.add").add_from_yank()
+    end, { desc = "Add snippet to CPKB from yank register" })
+
+    -- Map CPKB keys under <leader>c namespace
+    local map = vim.keymap.set
+    -- <leader>cs: Telescope Snippets Picker
+    map("n", "<leader>cs", "<cmd>Telescope cpkb<cr>", { desc = "CPKB Snippets Picker" })
     
-    -- Map Space+cs to open the snippet picker
-    vim.keymap.set("n", "<leader>cs", "<cmd>Telescope cpkb<cr>", { desc = "CPKB Snippets" })
+    -- <leader>cv: Add snippet from visual selection
+    map("v", "<leader>cv", function()
+      require("cpkb.add").add_from_visual()
+    end, { desc = "CPKB Add from Visual Selection" })
+
+    -- <leader>cy: Add snippet from yank register
+    map("n", "<leader>cy", function()
+      require("cpkb.add").add_from_yank()
+    end, { desc = "CPKB Add from Yank Register" })
   end,
 }
